78575963a790780d96c7584f7a783a796e9f20e7
Greg <gkjono@tervis.com>
2017-04-14 17:07:14 -0400

    Added Configuration Function

--- TervisDirectAccess.psm1
+++ TervisDirectAccess.psm1
@@ -5,3 +5,29 @@
     Invoke-ClusterApplicationProvision -ClusterApplicationName DirectAccess -EnvironmentName $EnvironmentName
     $Nodes = Get-TervisClusterApplicationNode -ClusterApplicationName DirectAccess -EnvironmentName $EnvironmentName
 }
+
+function Set-DirectAccessConfiguration {
+    param (
+        [Parameter(ValueFromPipelineByPropertyName)]$ComputerName
+    )
+    Begin {
+        $DirectAccessServerGpoName = $env:USERDNSDOMAIN + '\DirectAccess Server Settings'
+        $DirectAccessClientGpoName = $env:USERDNSDOMAIN + '\DirectAccess Client Settings'
+        $DirectAccessConnectToDomain = 'DirectAccess.' + $env:USERDOMAIN + '.com'
+        $DirectAccessNlsUrl = 'https://nls.' + $env:USERDOMAIN + '.com/'
+        $DirectAccessCorporateResources = 'HTTP:http://directaccess-WebProbeHost.' + $env:USERDOMAIN + '.com'
+    }
+    Process {
+        Invoke-Command -ComputerName $ComputerName -ScriptBlock {
+            Install-RemoteAccess -Force -PassThru -ServerGpoName $Using:DirectAccessServerGpoName -ClientGpoName $Using:DirectAccessClientGpoName -DAInstallType 'FullInstall' -InternetInterface 'Internet' -InternalInterface 'Internal' -ConnectToAddress $Using:DirectAccessConnectToDomain -NlsUrl $Using:DirectAccessNlsUrl
+
+            $certs = Get-ChildItem Cert:\LocalMachine\Root  
+            $IPsecRootCert = $certs | Where-Object {$_.Subject -Match "INF-DirectAcc"}  
+            Set-DAServer -IPsecRootCertificate $IPsecRootCert 
+
+            Set-DAClient -OnlyRemoteComputers 'Enabled' -Downlevel 'Enabled'
+
+            Set-DAClientExperienceConfiguration -FriendlyName 'Tervis DirectAccess Connection' -PreferLocalNamesAllowed $False -PolicyStore $Using:DirectAccessClientGpoName -CorporateResources @($Using:DirectAccessCorporateResources)
+        }
+    }
+}
