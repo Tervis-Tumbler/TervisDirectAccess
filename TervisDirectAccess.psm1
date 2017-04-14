e2b49c58a32a153f9ad036fc79374aaea6090f75
Greg <gkjono@tervis.com>
2017-04-14 08:40:04 -0400

    Initial Script Commit
    At this point it will only spin up the blank VMs after the necessary
    definitions are added to the modules.

---
+++ TervisDirectAccess.psm1
@@ -0,0 +1,7 @@
+﻿function Invoke-DirectAccessProvision {
+    param (
+        $EnvironmentName
+    )
+    Invoke-ClusterApplicationProvision -ClusterApplicationName DirectAccess -EnvironmentName $EnvironmentName
+    $Nodes = Get-TervisClusterApplicationNode -ClusterApplicationName DirectAccess -EnvironmentName $EnvironmentName
+}
